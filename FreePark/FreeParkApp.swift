d915be84b02f17fe2337c5b2e9931a627345b41b
Karim ElSedfy <kariemelsedfy@gmail.com>
2025-11-24 14:23:51 -0500

    Initial Commit

---
+++ FreePark/FreeParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeParkApp.swift
+//  FreePark
+//
+//  Created by Karim ElSedfy on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
